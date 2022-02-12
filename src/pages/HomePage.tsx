b0befa94f70ee5b3a6be4ea5c33c0aa4477b4ac1
Erik Pizana <erikpzcanedo@outlook.com>
2022-02-12 10:18:52 -0600

    Menu

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FlightSelector } from "../components/FlightSelector";
 import styles from "../styles/home-page.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const HomePage = () => {
   const { homeContainer, portada, header } = styles;
@@ -9,6 +11,9 @@ export const HomePage = () => {
       <div className={portada}>
         <header className={header}>
           <span>AEROLINEA</span>
+          <span style={{ marginLeft: "auto" }}>
+            <FontAwesomeIcon icon={faBars} />
+          </span>
           <a href="#">Clase negocios</a>
           <a href="#">Primera clase</a>
           <a href="#">Contacto</a>
